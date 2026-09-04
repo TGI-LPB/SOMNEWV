2e72ef87a4f3206b254ec8707e1df8f654866a44
TGI-LPB <toyskingdomlivingplazabintaro@gmail.com>
2026-09-04 14:46:18 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  // Masukkan Web App URL hasil deployment Google Apps Script Anda di sini
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwNbcRsqT06WlerFLerTO5ZEjEIiejpeODcnBM0HMF10AxFpszOnLgX3M0lhm63eUsd6g/exec',
-  DEBUG_MODE: true,
-  DEVICE_ID_KEY: 'SOM_DEVICE_UUID',
-  ACTIVE_LOCATION_KEY: 'SOM_ACTIVE_LOCATION',
-  SESSION_TOKEN_KEY: 'SOM_AUTH_TOKEN'
-};
