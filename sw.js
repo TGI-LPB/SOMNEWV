ab9b3fed0967f3f0e7787992aba23504643e2761
TGI-LPB <toyskingdomlivingplazabintaro@gmail.com>
2026-09-04 15:25:28 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "so-mandiri-v1";
+const CACHE_NAME = "so-mandiri-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -14,13 +14,9 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
