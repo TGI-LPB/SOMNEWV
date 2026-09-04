db5d6cdb2ccaeecc3048bb3269e6c1c08115947f
TGI-LPB <toyskingdomlivingplazabintaro@gmail.com>
2026-09-04 14:47:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,28 @@
 const CACHE_NAME = 'som-cache-v1';
-const STATIC_ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './css/app.css',
-  './js/config.js',
-  './js/db.js',
-  './js/api.js',
-  './js/auth.js',
-  './js/sync.js',
-  './js/ui.js',
-  './js/so.js',
-  './js/app.js',
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
   'https://cdn.tailwindcss.com',
-  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11',
+  'https://cdnjs.cloudflare.com/ajax/libs/localforage/1.10.0/localforage.min.js',
+  'https://unpkg.com/html5-qrcode'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+self.addEventListener('fetch', event => {
+  // Hanya intercept GET request (Abaikan POST API Google Script)
+  if (event.request.method === 'GET') {
+      event.respondWith(
+        caches.match(event.request).then(response => {
+          return response || fetch(event.request);
+        })
       );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) return cachedResponse;
-      return fetch(event.request).then((networkResponse) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      });
-    }).catch(() => caches.match('./index.html'))
-  );
+  }
 });
