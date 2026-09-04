1f5c60e81e39baa09e14b9b34b42a0ec2f6ae8ac
TGI-LPB <toyskingdomlivingplazabintaro@gmail.com>
2026-09-04 15:36:39 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
-const CACHE_NAME = "so-mandiri-v2";
+const CACHE_NAME = 'so-mandiri-v1';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./css/styles.css",
-  "./js/app.js",
-  "./js/db.js",
-  "./js/scanner.js",
-  "./js/sync.js",
-  "./manifest.json",
-  "https://cdn.tailwindcss.com",
-  "https://unpkg.com/html5-qrcode",
-  "https://cdn.jsdelivr.net/npm/@ericblade/quagga2/dist/quagga.min.js"
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/html5-qrcode',
+  'https://cdn.jsdelivr.net/npm/quagga@0.12.10/dist/quagga.min.js'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
-
